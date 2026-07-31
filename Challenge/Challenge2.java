82ef7213ad4cae925fe1b6e62bf7f9a6caeade7b
Zocken <118059829+Zackarylevi@users.noreply.github.com>
2026-07-31 06:28:46 +0000

    challenge

---
+++ Challenge/Challenge2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Challenge2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter month number (1-12): ");
+        int month = scanner.nextInt();
+
+        switch (month) {
+
+            case 1:  // January
+            case 3:  // March
+            case 5:  // May
+            case 7:  // July
+            case 8:  // August
+            case 10: // October
+            case 12: // December
+                System.out.println("Number of days: 31");
+                break;
+
+            case 4:  // April
+            case 6:  // June
+            case 9:  // September
+            case 11: // November
+                System.out.println("Number of days: 30");
+                break;
+
+            case 2:
+                System.out.println("Number of days: 28");
+                break;
+
+            default:
+                System.out.println("Invalid month number. Please enter a number between 1 and 12.");
+                break;
+        }
+
+        scanner.close();
+    }
+}
